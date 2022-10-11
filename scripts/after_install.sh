ddc70377394a043f110718d8540d9f9cb5b18259
Subhash Singh <subhash_singh@seologistics.com>
2022-10-11 16:48:36 +0530

    a

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/server
-npm install
-npm install pm2 -g
+#cd /home/ec2-user/server
+#npm install
+#npm install pm2 -g
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/server/
-pm2 start server.js --name "React-Server" 
+touch apple.txt
+#pm2 start server.js --name "React-Server" 
