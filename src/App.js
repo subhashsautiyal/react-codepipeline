00786298d955a2f3f3b9a85ce758954568d5fd3b
Subhash Singh <subhash_singh@seologistics.com>
2022-10-12 16:56:26 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> working Without Build In CodePipeLine.
+          Edit <code>src/App.js</code> SUbhash.
         </p>
         <a
           className="App-link"
