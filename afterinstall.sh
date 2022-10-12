07b867c9a0172e55398e939185dbcb6ac1a80fd1
Subhash Singh <subhash_singh@seologistics.com>
2022-10-12 10:33:09 +0530

    Initial Commit

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /home/ec2-user/server
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+yum -y install nodejs npm
 npm -f install
 npm run build
 npm install -g pm2
