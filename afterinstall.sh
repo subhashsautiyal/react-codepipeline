f0bed41673ea44adb6688bd381ead81b6c57bb7e
Subhash Singh <subhash_singh@seologistics.com>
2022-10-12 11:12:00 +0530

    a

--- afterinstall.sh
+++ afterinstall.sh
@@ -2,7 +2,16 @@
 
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-#yum -y install nodejs npm
-#npm -f install
-#npm run build
-#npm install -g pm2
+yum -y install nodejs npm
+
+
+#Remove Unused Code
+
+rm -rf node_modules
+rm -rf build
+
+#Install node_modules & Make Build
+
+npm -f install
+npm run build
+npm install -g pm2
