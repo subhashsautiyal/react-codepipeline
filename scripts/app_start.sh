1aeb9807be96504d464b19018cae7c9f908dd237
subhashsautiyal <68697760+subhashsautiyal@users.noreply.github.com>
2022-10-28 19:59:29 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -4,6 +4,7 @@
 cd /home/ec2-user/server
 
 #_Delete_Old_PM2_Service
-sudo pm2 delete Frontend
-sudo pm2 start server.js --name Frontend
-
+#sudo pm2 delete Frontend
+#sudo pm2 start server.js --name Frontend
+pm2 delete Frontend
+pm2 start server.js --name Frontend
